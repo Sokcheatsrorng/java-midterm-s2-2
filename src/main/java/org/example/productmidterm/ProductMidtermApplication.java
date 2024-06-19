785e9e93d4d957eec8c3ff1f6ceeff9442eb453f
Sokcheatsrorng <sokcheat.srorng@gmail.com>
2024-06-19 18:18:02 +0700

    initial

---
+++ src/main/java/org/example/productmidterm/ProductMidtermApplication.java
@@ -0,0 +1,13 @@
+package org.example.productmidterm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductMidtermApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProductMidtermApplication.class, args);
+    }
+
+}
